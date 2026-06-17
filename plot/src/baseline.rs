88772c59c413c5cfb9db47a22bd9f743ffe05d45
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-06-17 19:10:33 +0200

    first set of updated results, and updated figures

--- plot/src/baseline.rs
+++ plot/src/baseline.rs
@@ -1,39 +1,45 @@
 mod prelude;
 use prelude::*;
 use epy::prelude::*;
+use std::path::Path;
+
+pub fn read_csv(device: Device, baseline: Baseline) -> DataFrame<Record> {
+    let path = format!("../src/res/{}/{}.csv", device, baseline);
+    DataFrame::from_csv(Path::new(&path)).unwrap()
+}
 
 fn plot(df: &DataFrame<Record>) -> TikzPicture {
     let (mut ax0, mut ax1) = TwinPlot::new(
             |r: &Record| r.threads as f64,
             "Threads",
-            "Energy",
-            "Runtime",
+            "Energy (J)",
+            "Runtime (s)",
         )
         .ax0_bar(df,
             |r| r.energy(),
             AggregationMode::Quartiles,
-            "Energy",
+            "Energy (J)",
             "energycolor",
         )
         .ax1_line(df,
             |r| r.runtime(),
             AggregationMode::meanstd(),
-            "Runtime",
+            "Runtime (s)",
             "runtimecolor",
         )
         .build_axes();
     ax0.style.ymin = Some(0.0);
     ax1.style.ymin = Some(0.0);
+    remove_legend(&mut ax0);
+    ax0.style.filter_xticks(|i| filter_every(i, 3));
     TikzPicture::from_twin(ax0, ax1)
 }
 
 fn main() {
-    let df = read_csv("cn125/matmul");
-
-    let doc = plot(&df.clone().filter(|_, r| r.size == 500));
-    println!("{}", doc.render());
-    let doc = plot(&df.clone().filter(|_, r| r.size == 1000));
-    println!("{}", doc.render());
-    let doc = plot(&df.clone().filter(|_, r| r.size == 1500));
-    println!("{}", doc.render());
+    let df = read_csv(Device::CN125, Baseline::Matmul);
+    for size in unique_sizes(&df) {
+        let doc = plot(&df.clone().filter(|_, r| r.size == size));
+        let path = format!("../paper/img/{}_{}_{}.tex", Device::CN125, Baseline::Matmul, size);
+        doc.write(&path).unwrap();
+    }
 }
--- plot/src/prelude.rs
+++ plot/src/prelude.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 use epy::prelude::*;
 use serde::Deserialize;
-use std::{collections::HashSet, fmt, path::Path};
+use std::{collections::HashSet, fmt};
 
 #[derive(Clone, Deserialize)]
 pub struct Record {
@@ -9,6 +9,41 @@ pub struct Record {
     pub threads: usize,
     runtime: f64,
     energy: f64,
+}
+
+pub enum Device {
+    CN125,
+    Laptop,
+}
+
+impl fmt::Display for Device {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Device::*;
+        match self {
+            CN125 => write!(f, "cn125"),
+            Laptop => write!(f, "laptop"),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum Baseline {
+    Matmul,
+    Nbody,
+    Stencil,
+    Rust,
+}
+
+impl fmt::Display for Baseline {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use Baseline::*;
+        match self {
+            Matmul => write!(f, "matmul"),
+            Nbody => write!(f, "nbody"),
+            Stencil => write!(f, "stencil"),
+            Rust => write!(f, "rust"),
+        }
+    }
 }
 
 impl Record {
@@ -21,9 +56,12 @@ impl Record {
     }
 }
 
-pub fn read_csv(filename: &str) -> DataFrame<Record> {
-    let path = format!("../src/res/{}.csv", filename);
-    DataFrame::from_csv(Path::new(&path)).unwrap()
+pub fn max_threads(df: &DataFrame<Record>) -> usize {
+    df.rows()
+        .iter()
+        .map(|r| r.threads)
+        .max()
+        .unwrap()
 }
 
 pub fn unique_threads(df: &DataFrame<Record>) -> Vec<usize> {
@@ -37,10 +75,17 @@ pub fn unique_threads(df: &DataFrame<Record>) -> Vec<usize> {
     t
 }
 
-pub fn max_threads(df: &DataFrame<Record>) -> usize {
-    df.rows()
+pub fn unique_sizes(df: &DataFrame<Record>) -> Vec<usize> {
+    let mut t = df.rows()
         .iter()
-        .map(|r| r.threads)
-        .max()
-        .unwrap()
+        .map(|r| r.size)
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect::<Vec<_>>();
+    t.sort_unstable();
+    t
+}
+
+pub fn remove_legend(ax: &mut Axis) {
+    ax.data.retain(|e| !matches!(e, AxisElement::LegendEntry(_)));
 }
