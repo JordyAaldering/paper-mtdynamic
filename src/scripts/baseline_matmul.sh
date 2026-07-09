a8428c4866eacb4fc7b09d596fd7a6c258b5385f
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-07-09 10:53:36 +0200

    make for correct size

--- src/scripts/baseline_matmul.sh
+++ src/scripts/baseline_matmul.sh
@@ -8,13 +8,13 @@
 #SBATCH --time=10:00:00
 #SBATCH --output=baseline_matmul.out
 
-make bin/matmul_mt
-
 echo "size,threads,runtime,energy" > res/baseline_matmul.csv
 
 for size in 500 1000 1500; do
+    make bin/matmul_$size
+
     for threads in $(seq 16); do
-      SAC_PARALLEL=$threads taskset -c 0-$(($threads-1)) ./bin/matmul_mt $size \
+      SAC_PARALLEL=$threads taskset -c 0-$(($threads-1)) ./bin/matmul_$size \
         | awk -v size=$size -v threads=$threads '{
             printf "%d,%d,%s\n", size, threads, $0;
         }' >> res/baseline_matmul.csv
